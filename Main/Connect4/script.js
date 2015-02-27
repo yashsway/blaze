cb819cc4622b6a3e1be35cb6da652e92c082f916
Yash Kadaru <reach.yashg@hotmail.com>
2015-02-26 19:22:53 -0500

    Playable Version 2.1
    - Added grid.
    - Changed player color's and name's
    - Reorganized HTML/CSS better.
    - Reorganized script.js functions
    - Added randomizer that randomizes which player goes first.

--- Main/Connect4/script.js
+++ Main/Connect4/script.js
@@ -3,52 +3,53 @@ var columns = [[], [], [], [], [], [], []];
 var launch = false;
 var reset = false;
 var redChips = 21;
-var yellowChips = 21; //42 in total. (7x6 grid)
-var currentPlayer = 0;
+var blueChips = 21; //42 in total. (7x6 grid)
+var currentPlayer = Math.round((Math.random()));
+var mode = "play";
 
 //Calculates how far to drop
 var dropLength = function(index) {
     //6 Chips + some buffer space - length of the column (# of chips in the argument column).. all * 100. (Each chip is 100x100px)
-    //window.alert(((6 + 0.20) - columns[index].length) * 100);
     return ((6 + 0.20) - columns[index].length) * 100;
 }
 
-//Makes the grid of the Connect 4 game (6 x 7 grid)
-function boardMaker() {
-    var mod = 0;
-    //Horizontal Lines (width: 700px)
-    for (i = 0; i < 7; i++) {
-        var line = new createjs.Shape();
-        line.graphics.beginStroke("red");
-        line.graphics.moveTo(200, 100 + mod).lineTo(900, 100 + mod); //moveTo(x,y) -> lineTo(x,y)
-        stage.addChild(line);
-        mod = mod + 100;
-    }
-    mod = 0;
-    //Vertical Lines (height: 600)
-    for (i = 0; i < 8; i++) {
-        var line = new createjs.Shape();
-        line.graphics.beginStroke("red");
-        line.graphics.moveTo(200 + mod, 100).lineTo(200 + mod, 700); //moveTo(x,y) -> lineTo(x,y)
-        stage.addChild(line);
-        mod = mod + 100;
-    }
-    //Update the stage with all the lines
-    stage.update();
+//Hides both player pointers
+function hidePlayerPointers(){
+    $(".player-icons.play#p1").hide();
+    $(".player-icons.play#p2").hide();
+};
+
+//Display the message specified in the argument. Also run an exit animation on it after 5s.
+function messageConsole(selector){
+    $(selector).show();
+    $(selector).delay(5000).slideUp("slow");
 }
 
+//Updates the current player arrow icons based on player turn
+var updateCurrentPlayer = function() {
+    //p contains the next player
+    var p = currentPlayer%2 ? "player2" : "player1";
+    if(p=="player1"){
+        $(".player-icons.play#p1").show();
+        $(".player-icons.play#p2").hide();
+    }else if(p="player2"){
+        $(".player-icons.play#p2").show();
+        $(".player-icons.play#p1").hide();
+    }
+};
+
+//Checks if the game has ended in a draw.
 var checkForDraw = function() {
     //Traverse through all the 7 columns
     for(var i = 0; i < columns.length; i++)
         //And check if all the columns are NOT full
         if(columns[i].length < 6)
             return;
-    //If they are full...
-    //$(".displaycontainer h2").text("Draw!");
-    //Display win assets.
-    //$(".win").show();
-    window.alert("DRAW");
-    //Stop game
+    //If they are full...display a draw message
+    messageConsole("#draw");
+    //Hide player pointers..
+    hidePlayerPointers();
+    //and stop the game
     launch = false;
 };
 
@@ -62,6 +63,7 @@ var locate = function(type, x, y) {
     return (columns[x][y] === type);
 };
 
+//Checks if the current chip has 3 in a row of the same type next to it.
 function winner(type,x,y) {
     //window.alert(!locate(type, x, y));
     if(!locate(type, x, y)) return false;
@@ -83,31 +85,90 @@ function winner(type,x,y) {
     }
     return false;
 };
+//Play and Developer Mode button toggle + player selector animations + player selector
+$(document).ready(function(){
+    $("#play-mode").bind("click", function() {
+        mode = "play";
+        $(".dev-mode").hide();
+        $(".mode-icon#play-mode-icon").show();
+        $(".mode-icon#dev-mode-icon").hide();
+        $("#play-mode").css({"opacity":0.25});
+        $("#dev-mode").css({"opacity":1});
+        $("#play-mode").prop('disabled',true);
+        $("#dev-mode").prop('disabled',false);
+    });
+    $("#dev-mode").bind("click", function() {
+        mode = "dev";
+        $(".dev-mode").show();
+        $(".mode-icon#dev-mode-icon").show();
+        $(".mode-icon#play-mode-icon").hide();
+        $("#dev-mode").css({"opacity":0.25});
+        $("#play-mode").css({"opacity":1});
+        $("#dev-mode").prop('disabled',true);
+        $("#play-mode").prop('disabled',false);
+    });
+    $(".playerCircle.p1").hover(function() {
+        $(this).fadeTo("fast",1);
+    },function() {
+            $(this).fadeTo("slow",0.5);
+    });
+    $(".playerCircle.p2").hover(function() {
+        $(this).fadeTo("fast",1);
+    },function() {
+            $(this).fadeTo("slow",0.5);
+    });
+});
+
+//Main Function
+$(document).ready(function init(){
+    //Hide player pointers
+    hidePlayerPointers();
+    //Display a welcome message
+    messageConsole("#welcome");
+    //Stage instance (GLOBAL)
+    stage = new createjs.Stage("game-canvas");
+    //Make the game board
+    horizontalLineMaker();
+    //boardMaker();
+    //When the start button is clicked...
+    $("#launch").click(function() {
+        updateCurrentPlayer();
+        $("#launch").prop('disabled',true);
+        launch = true;
+        messageConsole("#inProgress");
+    });
+});
 
 //Updates the game after every chip play
 var updateGame = function(index) {
-    //If NOT playing at this moment, don't do anything in this function
-    //if(!inPlay) return false;
     //Calculate how many empty spaces are available in this column
     var colLength = columns[index].length;
-    //window.alert(colLength);
     //If the column is full of chips...
     if(colLength >= 6) return false;
-    //Else...Push the chip type into the respective column array (0=red, 1=yellow)
+    //Else...Push the chip type into the respective column array (0=blue, 1=red)
     var type = currentPlayer % 2;
     columns[index].push(type);
     //and increment the current player
     currentPlayer++;
+    //Now check if the chip that you just placed is the winning entry...
     if(winner(type, index, colLength)) {
         //If the winner function returned true, one of the players has won. Display the win assets.
+        //If blue won...
         if(type==0){
+            //Display an informative message
             messageConsole("#player1Win");
-            $(".win.icon#p1").show();
-            $(".current-play#p1").hide();
+            //Show a visual indicator
+            $(".player-icons.win#p1").show();
+            //Hide the player pointers
+            hidePlayerPointers();
+        //If red won...
         }else if(type==1){
+            //Display an informative message
             messageConsole("#player2Win");
-            $(".win.icon#p2").show();
-            $(".current-play#p2").hide();
+            //Show a visual indicator
+            $(".player-icons.win#p2").show();
+            //Hide the player pointers
+            hidePlayerPointers();
         }
         //Game not in play anymore.
         launch = false;
@@ -118,68 +179,36 @@ var updateGame = function(index) {
     return true;
 };
 
-//Updates the current player arrow icons based on player turn
-var updateCurrentPlayer = function() {
-    //p contains the next player
-    var p = currentPlayer%2 ? "player2" : "player1";
-    if(p=="player1"){
-        $(".current-play#p1").show();
-        $(".current-play#p2").hide();
-    }else if(p="player2"){
-        $(".current-play#p2").show();
-        $(".current-play#p1").hide();
-    }
-};
-
-//Hides all pointer/win assets
-function hidePointers(){
-    //Hide the current player indicators
-    $(".current-play#p1").hide();
-    $(".current-play#p2").hide();
-    //Hide the win assets
-    $(".win").hide();
-}
-
-function messageConsole(selector){
-    $(selector).show();
-    $(selector).delay(5000).slideUp("slow");
-}
-
-//Run this function on page load
-$(document).ready(function init(){
-    messageConsole("#welcome");
-    //Stage instance (GLOBAL)
-    stage = new createjs.Stage("game-canvas");
-    //Make the game board
-    //boardMaker();
-    //FOLLOWING EVENT IS A TEST EVENT
-    $("#info-panel #game-tools-panel #launch").click(function() {
-        updateCurrentPlayer();
-        $("#launch").prop('disabled',true);
-        launch = true;
-        $("#inProgress").show();
-        messageConsole("#inProgress");
-    });
+//The following function adds chips to a clicked column (PLAY Mode)
+$(document).ready(function chipPlacer(){
     $(".columncontainer").click(function() {
         //If game hasn't been launched yet
         if(launch==false) return;
+        //If game is NOT in play mode
+        if(mode=="dev") return;
+        //Store the index of the object that was clicked
         var index = $(this).index();
-        //Update the game (Also the point where game doesn't execute when not in play)
+        //Update the game (Also the point where you cannot enter any more chips when a column is full)
         if(!updateGame(index)) return;
-        //set p to be the current player's ID (0=red,1=yellow)
+        //set p to be the current player's ID (0=blue,1=red)
         var p = currentPlayer%2 ? "player1":"player2";
         //Make a new chip object
         var newChip = "<div class=\"chip " + p + "\"></div>";
-        //Add the chip to the current columncontainer
+        //Add the chip object to the current columncontainer
         $(this).prepend(newChip);
-        var t = $(this).children(".chip:first-child").position().top;
-        $(this).children(".chip:first-child").css("top", t);
-        $(this).children(".chip:first-child").animate({top:"+="+dropLength(index)+"px",opacity:'0.5'},400);
+        //var t = $(this).children(".chip:first-child").position().top;
+        //$(this).children(".chip:first-child").css("top", t);
+        //Take that recently added chip object and animate it to drop to the bottom (refer to the dropLength function as well)
+        $(this).children(".chip:first-child").animate({top:"+="+dropLength(index)+"px",opacity:'0.6'},400);
         //If game has ended.... don't call updateDisplayChip()
         if(!launch) return;
         //Update the current player pointers
         updateCurrentPlayer();
     });
+});
+
+//The following function programs the reset button to wait for a click event
+$(document).ready(function reset(){
     //Now we program the reset button
     $("#reset").click(function() {
         //Fade out all the chips
@@ -188,13 +217,55 @@ $(document).ready(function init(){
         });
         //Reset the array
         for(var i = 0; i < columns.length; i++) columns[i] = [];
-        //Game has been reset. Set the default player back to red.
-        currentPlayer = 0;
+        //Game has been reset. Set the default player to either Red or Blue
+        currentPlayer = Math.round((Math.random()));
         //Set the launch key back to false
         launch = false;
         //Enable the start button
         $("#launch").prop('disabled',false);
-        //Hide all indicators
-        hidePointers();
+        //Hide the player pointers
+        hidePlayerPointers();
+        //Hide the win assets
+        $(".win").hide();
+        //Display message to user
+        messageConsole("#resetMsg");
     });
 });
+
+function horizontalLineMaker() {
+    var mod = 0;
+    //Horizontal Lines (width: 700px)
+    for (i = 0; i < 5; i++) {
+        var line = new createjs.Shape();
+        line.graphics.beginStroke("#724EB1");
+        line.graphics.moveTo(0, 100 + mod).lineTo(900, 100 + mod); //moveTo(x,y) -> lineTo(x,y)
+        stage.addChild(line);
+        mod = mod + 100;
+    }
+    stage.update();
+}
+
+//Makes the grid of the Connect 4 game (6 x 7 grid)
+/*function boardMaker() {
+    var mod = 0;
+
+    //Horizontal Lines (width: 700px)
+    for (i = 0; i < 7; i++) {
+        var line = new createjs.Shape();
+        line.graphics.beginStroke("red");
+        line.graphics.moveTo(200, 100 + mod).lineTo(900, 100 + mod); //moveTo(x,y) -> lineTo(x,y)
+        stage.addChild(line);
+        mod = mod + 100;
+    }
+    mod = 0;
+    //Vertical Lines (height: 600)
+    for (i = 0; i < 8; i++) {
+        var line = new createjs.Shape();
+        line.graphics.beginStroke("red");
+        line.graphics.moveTo(200 + mod, 100).lineTo(200 + mod, 700); //moveTo(x,y) -> lineTo(x,y)
+        stage.addChild(line);
+        mod = mod + 100;
+    }
+    //Update the stage with all the lines
+    stage.update();
+}*/
